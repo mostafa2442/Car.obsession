c2e53186c38d1eeb7e88aeba443cfc2ebbe4e690
mostafa2442 <104231649+mostafa2442@users.noreply.github.com>
2023-05-05 00:47:18 +0300

    Add files via upload

---
+++ cars.js
@@ -0,0 +1,25 @@
+$(document).ready(function()
+{
+$("#appear1").click (function()
+{
+$(".example-one").toggle(2000);
+});
+});
+
+
+$(document).ready(function()
+{
+$("#appear2").click (function()
+{
+$(".example-2").toggle(2000);
+});
+});
+
+
+$(document).ready(function()
+{
+$("#appear3").click (function()
+{
+$(".example-3").toggle(2000);
+});
+});
